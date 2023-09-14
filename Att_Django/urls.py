11ffb30ab74f465eb093c45ae5c3b1bdb9e90b4e
LucasSobreira <lucasdantassodreira@gmail.com>
2023-09-14 20:48:24 -0300

    Pag Hello Word

--- Att_Django/urls.py
+++ Att_Django/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views # Importa as views do app core
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.hello)
 ]
